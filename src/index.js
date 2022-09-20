d0b078d093c051bc47341d4436303853985b5ce7
timur <timurka.zavyalov@mail.ru>
2022-09-20 13:40:16 +0300

    git init

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import 'antd/dist/antd.min.css';
 import 'react-diff-view/style/index.css';
 import './styles.css';
 import tokenize from './tokenize';
+import {run} from './animate.js'
+import PizZip from "pizzip"
 import Docxtemplater from "docxtemplater";
 
 
@@ -48,6 +50,7 @@ function App() {
     const [oldText, setTextValue] = useState('');
     async function handleChange(e) {
         const file = { file: e.target.files[0].name };
+        run()
         console.log(file);
         let response = await fetch("http://127.0.0.1:5000", {
             method: 'POST',
@@ -60,19 +63,21 @@ function App() {
         setTextValue(new_file['text']);
     };
 
+
     // insert newText
-    const [newText, setTextValue2] = useState('');
-    const handleChange2 = (e) => {
-        const file = e.target.files[0];
-        let reader = new FileReader();
-        reader.onload = (e) => {
-            const file = e.target.result;
-            console.log(file);
-            setTextValue2(file);
-        };
-        reader.onerror = (e) => alert(e.target.error.name);
-        reader.readAsText(file);
-    };
+    // const [newText, setTextValue2] = useState('');
+    // const handleChange2 = (e) => {
+    //     const file = e.target.files[0];
+    //     document.getElementById('pdf_files').placeholder='Loading...';
+    //     let reader = new FileReader();
+    //     reader.onload = (e) => {
+    //         const file = e.target.result;
+    //         console.log(file);
+    //         setTextValue2(file);
+    //     };
+    //     reader.onerror = (e) => alert(e.target.error.name);
+    //     reader.readAsText(file);
+    // };
 
     // // insert Jpg text
     // const [oldText, setTextValue] = useState('');
@@ -105,20 +110,20 @@ function App() {
     //     reader.readAsBinaryString(file);
     // };
 
-    // // insert Docx text
-    // const [newText, setTextValue2] = useState('');
-    // const handleChange2 = (e) => {
-    //     const file = e.target.files[0];
-    //     const reader = new FileReader();
-    //     reader.onload = async (e) => {
-    //         const content = e.target.result;
-    //         var doc = new Docxtemplater(new PizZip(content), {delimiters: {start: '12op1j2po1j2poj1po', end: 'op21j4po21jp4oj1op24j'}});
-    //         var text = doc.getFullText();
-    //         console.log(text);
-    //         setTextValue2(text);
-    //     };
-    //     reader.readAsBinaryString(file);
-    // };
+    // insert Docx text
+    const [newText, setTextValue2] = useState('');
+    const handleChange2 = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.onload = async (e) => {
+            const content = e.target.result;
+            var doc = new Docxtemplater(new PizZip(content), {delimiters: {start: '12op1j2po1j2poj1po', end: 'op21j4po21jp4oj1op24j'}});
+            var text = doc.getFullText();
+            console.log(text);
+            setTextValue2(text);
+        };
+        reader.readAsBinaryString(file);
+    };
 
 
     const updateDiffText = useCallback(() => {
@@ -134,8 +139,8 @@ function App() {
                 <input type="file" className="text" onChange={handleChange} />
                 <input type="file" className="text" onChange={handleChange2} />
                 <div className="input">
-                    <Input.TextArea className="text" value={oldText} onChange={setTextValue} rows={10} placeholder="old text..." {...oldText} />
-                    <Input.TextArea className="text" value={newText} onChange={setTextValue2} rows={10} placeholder="new text..." {...newText} />
+                    <Input.TextArea className="text" id='pdf_files' value={oldText} onChange={setTextValue} rows={10} placeholder="" {...oldText} />
+                    <Input.TextArea className="text" id='pdf_files' value={newText} onChange={setTextValue2} rows={10} placeholder="" {...newText} />
                 </div>
                 <Button className="submit" type="primary" onClick={updateDiffText}> 
                     GENERATE DIFF
