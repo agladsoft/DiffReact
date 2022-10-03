8f4b44a22910645b1204132316813d406450addb
timur <timurka.zavyalov@mail.ru>
2022-10-03 13:07:31 +0500

    fix: changed host

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ function App() {
             const dictFile = {file: [file.name, result] }
             console.log(dictFile)
             run()
-            let response = await fetch("http://127.0.0.1:5000", {
+            let response = await fetch("http://10.23.4.205:5000", {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(dictFile),
