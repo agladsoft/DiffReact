ce2c736a0e319fe27f7c5c70fd86a8af807ef095
timur <timurka.zavyalov@mail.ru>
2022-10-21 14:29:01 +0600

    add:  added  the ability to edit in the textarea

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, {useState, useCallback, useMemo} from 'react';
 import ReactDOM from 'react-dom';
 import {Input, Button} from 'antd';
+import { DownloadOutlined, PoweroffOutlined } from '@ant-design/icons';
 import {diffLines, formatLines} from 'unidiff';
 import {parseDiff, Diff, Hunk} from 'react-diff-view';
 import 'antd/dist/antd.min.css';
@@ -8,15 +9,13 @@ import 'react-diff-view/style/index.css';
 import './styles.css';
 import tokenize from './tokenize';
 import {run} from './animate.js'
-import PizZip from "pizzip"
-import Docxtemplater from "docxtemplater";
+import PizZip, { file } from "pizzip"
 
 const EMPTY_HUNKS = [];
 
 const renderToken = (token, defaultRender, i) => {
     switch (token.type) {
         case 'space':
-            console.log(token);
             return (
                 <span key={i} className="space">
                     {token.children && token.children.map((token, i) => renderToken(token, defaultRender, i))}
@@ -30,25 +29,52 @@ const renderToken = (token, defaultRender, i) => {
 function App() {
     const [{type, hunks}, setDiff] = useState('');
 
+    const useInput = (value, onChange) => {
+        return {
+            value,
+            onChange(e) {
+                onChange(e.target.value);
+            },
+        };
+    };
+
     // insert Docx text
-    const [oldText, setTextValue] = useState('');
+    const [value, onChange] = useState('');
+    function str2xml(str) {
+        if (str.charCodeAt(0) === 65279) str = str.substr(1);
+        return new DOMParser().parseFromString(str, "text/xml");
+    }
     const handleChange = (e) => {
-        const file = e.target.files[0];
+        var file = e.target.files[0];
         const reader = new FileReader();
         reader.onload = async (e) => {
             const content = e.target.result;
-            var doc = new Docxtemplater(new PizZip(content), {delimiters: {start: '12op1j2po1j2poj1po', end: 'op21j4po21jp4oj1op24j'}});
-            var text = doc.getFullText();
-            console.log(text);
-            setTextValue(text);
+            const zip = new PizZip(content);
+            const xml = str2xml(zip.files["word/document.xml"].asText());
+            const paragraphsXml = xml.getElementsByTagName("w:p");
+            var paragraphs = [];
+
+            for (let i = 0, len = paragraphsXml.length; i < len; i++) {
+                let fullText = "";
+                const textsXml = paragraphsXml[i].getElementsByTagName("w:t");
+                for (let j = 0, len2 = textsXml.length; j < len2; j++) {
+                    const textXml = textsXml[j];
+                    if (textXml.childNodes) {
+                        fullText += textXml.childNodes[0]?.nodeValue;
+                }
+                }
+                paragraphs.push(fullText.trim());
+            }
+            paragraphs = paragraphs.filter(Boolean)
+            onChange(paragraphs.join('\n'))
         };
         reader.readAsBinaryString(file);
     };
 
     // insert PDF text
-    const [newText, setTextValue2] = useState('');
+    const [valueNew, onChangeNew] = useState('');
     async function handleChange2(e) {
-        setTextValue2("");
+        onChangeNew("");
         const file = e.target.files[0];
         const fileReader = new FileReader();
         fileReader.addEventListener('load', ()=> {
@@ -57,68 +83,53 @@ function App() {
         async function loadPDF(result) {
             const key = file.name
             const dictFile = {[key]: result }
-            console.log(dictFile)
             run()
-            let response = await fetch("http://10.23.4.205:5000", {
+            let response = await fetch("http://127.0.0.1:5000", {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(dictFile),
                 mode: 'cors'
             });
             const new_file = await response.json();
-            console.log(new_file['text']);
-            setTextValue2(new_file['text']);
+            onChangeNew(new_file['text']);
         }
         fileReader.readAsDataURL(file)
     };
+    const oldText = useInput(value, onChange);
+    const newText = useInput(valueNew, onChangeNew);
 
 
     const updateDiffText = useCallback(() => {
-        const diffText = formatLines(diffLines(oldText, newText), {context: 3});
+        const diffText = formatLines(diffLines(oldText.value, newText.value), {context: 3});
         const [diff] = parseDiff(diffText, {nearbySequences: 'zip'});
         setDiff(diff);
-    }, [oldText, newText, setDiff]);
+    }, [oldText.value, newText.value, setDiff]);
     const tokens = useMemo(() => tokenize(hunks), [hunks]);
-    console.log(tokens);
 
-    // function SplitByString(source, splitBy) {
-    //     var splitter = splitBy.split('');
-    //     splitter.push([source]); //Push initial value
-      
-    //     return splitter.reduceRight(function(accumulator, curValue) {
-    //       var k = [];
-    //       accumulator.forEach(v => k = [...k, ...v.split(curValue)]);
-    //       return k;
-    //     });
-    //   }
 
-    // try {
-    //     for (let i = 0; i < tokens['new'][0].length; i++) {
-    //         console.log(tokens['new'][0][i]);
-    //         if (tokens['new'][0][i]['type'] === 'text') {
-    //             var splitBy = ",. ";
-    //             const array_text = SplitByString(tokens['new'][0][i]['value'], splitBy);
-    //             console.log(array_text.slice(array_text.length - 2, array_text.length))
-    //         }
-    //         else {
-    //             // console.log(tokens['new'][0][i]['lineNumber'])
-    //             console.log(tokens['new'][0][i]['children'][0]['value'])
-    //         }
-    //       }
-    //   } catch (error) {
-    //     console.error(error);
-    // }
-
+    function downloadReport() {
+        const dictFile = {"docx": oldText.value, "pdf": newText.value }
+        let response = fetch("http://127.0.0.1:5000/download", {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(dictFile),
+                mode: 'cors'
+        });
+        console.log(response)
+    }
     return (
         <div>
             <header className="header">
                 <input type="file" className="text" onChange={handleChange} />
                 <input type="file" className="text" onChange={handleChange2} />
                 <div className="input">
-                    <Input.TextArea className="text" id='pdf_files' value={oldText} onChange={setTextValue} rows={10} placeholder="" {...oldText} />
-                    <Input.TextArea className="text" id='pdf_files2' value={newText} onChange={setTextValue2} rows={10} placeholder="" {...newText} />
+                    <Input.TextArea className="text" id='pdf_files' value={oldText} rows={18} placeholder="" {...oldText} />
+                    <Input.TextArea className="text" id='pdf_files2' value={newText} rows={18} placeholder="" {...newText} />
                 </div>
-                <Button className="submit" type="primary" onClick={updateDiffText}> 
+                <Button className="submit" size="middle" shape="round" type="primary" icon={<DownloadOutlined />} onClick={downloadReport} >
+                    DOWNLOAD
+                </Button>
+                <Button className="submit" type="primary" icon={<PoweroffOutlined />} onClick={updateDiffText}> 
                     GENERATE DIFF
                 </Button>
             </header>
--- src/tokenize.js
+++ src/tokenize.js
@@ -3,26 +3,29 @@ import {flatMap} from 'lodash';
 
 const TOKEN_TYPE_SPACE = 'space';
 
-const findLeadingRange = change => {
-    const [spaces] = /^\s*/.exec(change.content);
-    return spaces
-        ? {
-              type: TOKEN_TYPE_SPACE,
-              lineNumber: change.lineNumber,
-              start: 0,
-              length: spaces.length,
-              properties: {value: spaces},
-          }
-        : null;
-};
+function getIndicesOf(searchStr, str, caseSensitive) {
+    var searchStrLen = searchStr.length;
+    if (searchStrLen === 0) {
+        return [];
+    }
+    var startIndex = 0, index, indices = [];
+    if (!caseSensitive) {
+        str = str.toLowerCase();
+        searchStr = searchStr.toLowerCase();
+    }
+    while ((index = str.indexOf(searchStr, startIndex)) > -1) {
+        indices.push(index);
+        startIndex = index + searchStrLen;
+    }
+    return indices;
+}
 
-const findTrailingRange = change => {
-    const [spaces] = /\s*$/.exec(change.content);
+const findLeadingRangeSpace = (spaces, change, pos) => {
     return spaces
         ? {
               type: TOKEN_TYPE_SPACE,
               lineNumber: change.lineNumber,
-              start: change.content.length - spaces.length,
+              start: pos,
               length: spaces.length,
               properties: {value: spaces},
           }
@@ -33,19 +36,21 @@ const pickLeadingAndTrailingSpaces = hunks => {
     const changes = flatMap(hunks, hunk => hunk.changes);
     const [oldRanges, newRanges] = changes.reduce(
         ([oldRanges, newRanges], change) => {
-            const leadingRange = findLeadingRange(change);
-            const trailingRange = findTrailingRange(change);
-            const pushRange = ranges => {
-                leadingRange && ranges.push(leadingRange);
-                trailingRange && ranges.push(trailingRange);
-            };
+            const [spaces] = /\s/.exec(change);
+            var indices = getIndicesOf(spaces, change.content);
+            for (const pos of indices) {
+                const leadingRangeSpace = findLeadingRangeSpace(spaces, change, pos);
+                const pushRange = ranges => {
+                    leadingRangeSpace && ranges.push(leadingRangeSpace);
+                };
 
-            if (!change.isInsert) {
-                pushRange(oldRanges);
-            }
-            if (!change.isDelete) {
-                pushRange(newRanges);
-            }
+                if (!change.isInsert) {
+                    pushRange(oldRanges);
+                }
+                if (!change.isDelete) {
+                    pushRange(newRanges);
+                }
+              }
 
             return [oldRanges, newRanges];
         },
@@ -58,10 +63,12 @@ export default hunks => {
     if (!hunks) {
         return undefined;
     }
-
     const options = {
         highlight: false,
-        enhancers: [markEdits(hunks, {type: 'block'}), pickLeadingAndTrailingSpaces(hunks)],
+        enhancers: [
+            markEdits(hunks, {type: 'block'}),
+            pickLeadingAndTrailingSpaces(hunks)
+        ]
     };
 
     try {
