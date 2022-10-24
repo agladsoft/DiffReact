9ce856d9c3d7d68ab2357f98cb9843af2859ec8e
timur <timurka.zavyalov@mail.ru>
2022-10-24 15:02:44 +0500

    add: catching errors

--- src/index.js
+++ src/index.js
@@ -117,6 +117,19 @@ function App() {
                 mode: 'cors'
             });
         console.log(response)
+        response
+        .then((response) => {
+            if (!response.ok) {
+              throw new Error(`HTTP error: ${response.status}`);
+            }
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data[0].name);
+          })
+          .catch((error) => {
+            console.error(`Could not get products: ${error}`);
+          });
     }
 
     return (
