a2a8a04e7670c4aec5dac48c7c06b2a06b5b929e
timur <timurka.zavyalov@mail.ru>
2022-11-09 13:58:58 +0500

    fix: changed ip

--- src/index.js
+++ src/index.js
@@ -150,7 +150,7 @@ function App() {
             const dictFile = {[key]: result };
             $('#pdf_files2').attr("placeholder", "Загрузка...");
             console.log("Length of binary pdf file", dictFile[key].length);
-            let response = await fetch("http://127.0.0.1:5000", {
+            let response = await fetch("http://10.23.4.205:5000", {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(dictFile),
